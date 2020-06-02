27ef319cd0812969c58a9f293ae22d501f54df88
Luke <luke.wyeth@mail.com>
2020-06-03 10:53:05 +1200

    Add more sort functions
    sort alphabetically by reverse now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,117 +13,130 @@ import DropdownButton from 'react-bootstrap/DropdownButton'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class MessageApp extends Component {
-  constructor(){
+  constructor() {
     super()
     this.state = {
       articles: []
     }
   }
 
-  /* added by Luke */
-  orderByYearDesc(){
-    const sorted = [...this.state.articles].filter(article => typeof article.year != 'undefined').sort((a,b) => b.year - a.year);
+  /* Start of sort functions */
+
+  orderByYearDesc() {
+    const sorted = [...this.state.articles].filter(article => typeof article.year != 'undefined').sort((a, b) => b.year - a.year);
     this.setArticles(sorted);
   }
 
-  /* added by Luke */
-  orderByYearAsc(){
-    const sorted = [...this.state.articles].filter(article => typeof article.year != 'undefined').sort((a,b) => a.year - b.year);
+  orderByYearAsc() {
+    const sorted = [...this.state.articles].filter(article => typeof article.year != 'undefined').sort((a, b) => a.year - b.year);
     this.setArticles(sorted);
   }
 
-   /* added by Luke */
   orderByTitleAlphabetical() {
     const sorted = [...this.state.articles].filter(article => typeof article.title != 'undefined').sort((a, b) => (a.title > b.title) ? 1 : -1);
     this.setArticles(sorted);
   }
 
-   /* added by Luke */
+  orderByTitleAlphabeticalReverse() {
+    const sorted = [...this.state.articles].filter(article => typeof article.title != 'undefined').sort((a, b) => (a.title < b.title) ? 1 : -1);
+    this.setArticles(sorted);
+  }
+
   orderBySourceAlphabetical() {
     const sorted = [...this.state.articles].filter(article => typeof article.source != 'undefined').sort((a, b) => (a.source > b.source) ? 1 : -1);
     this.setArticles(sorted);
   }
 
-   /* added by Luke */
+  orderBySourceAlphabeticalReverse() {
+    const sorted = [...this.state.articles].filter(article => typeof article.source != 'undefined').sort((a, b) => (a.source < b.source) ? 1 : -1);
+    this.setArticles(sorted);
+  }
+
   orderByAuthorAlphabetical() {
     const sorted = [...this.state.articles].filter(article => typeof article.author != 'undefined').sort((a, b) => (a.author > b.author) ? 1 : -1);
     this.setArticles(sorted);
   }
 
+  orderByAuthorAlphabeticalReverse() {
+    const sorted = [...this.state.articles].filter(article => typeof article.author != 'undefined').sort((a, b) => (a.author < b.author) ? 1 : -1);
+    this.setArticles(sorted);
+  }
 
-  setError(error){
+  /* End of sort functions */
+
+  setError(error) {
     this.setState({
       error: error
     })
   }
 
-  setArticles(articles){
+  setArticles(articles) {
     this.setState({
       articles: articles
     })
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getAllMessages()
   }
 
-  getAllMessages=()=>{
+  getAllMessages = () => {
     axios.get(`/getarticles`)
-    .then((result)=>{
-      console.log("hi" + result.data)
-      this.setArticles(result.data)
-    })
-    .catch((err)=>{
-      this.setError(err)
-    })
+      .then((result) => {
+        console.log("hi" + result.data)
+        this.setArticles(result.data)
+      })
+      .catch((err) => {
+        this.setError(err)
+      })
   }
 
   searchMessage = (keyword) => {
     axios.get(`/search/${keyword}`, {
       keyword: keyword
     })
-    .then((result)=>{
-      this.setArticles(result.data)
-    })
-    .catch((err)=>{
-      this.setError(err)
-    })
+      .then((result) => {
+        this.setArticles(result.data)
+      })
+      .catch((err) => {
+        this.setError(err)
+      })
   }
 
   submitMessage = (data) => {
     axios.post(`/submit`, {
       content: data
     })
-    .then(()=>{
-      this.getAllMessages()
-    })
-    .catch((err)=>{
-      this.setError(err)
-    })
+      .then(() => {
+        this.getAllMessages()
+      })
+      .catch((err) => {
+        this.setError(err)
+      })
   }
 
   deleteMessage = (id) => {
     axios.delete(`/delete/${id}`, {
       id: id
     })
-    .then((result)=>{
-      this.getAllMessages()
-    })
-    .catch((err)=>{
-      this.setError(err);
-    })
+      .then((result) => {
+        this.getAllMessages()
+      })
+      .catch((err) => {
+        this.setError(err);
+      })
   }
 
   sendUpdate = (id, content) => {
     axios.put(`/update/${id}`, {
       content: content
     })
-    .then((result)=>{
-      this.getAllMessages()
-    })
-    .catch((err)=>{
-      this.setError(err);
-    })
+      .then((result) => {
+        this.getAllMessages()
+      })
+      .catch((err) => {
+        this.setError(err);
+      })
   }
 
   /* added by luke */
@@ -132,51 +145,54 @@ class MessageApp extends Component {
     axios.post(`/submit`, {
       article: data
     })
-    .then(()=>{
-      this.getAllMessages()
-    })
-    .catch((err)=>{
-      this.setError(err)
-    })
+      .then(() => {
+        this.getAllMessages()
+      })
+      .catch((err) => {
+        this.setError(err)
+      })
   }
 
-  render(){
+  render() {
     return (
       <React.Fragment>
         <Navbar className="justify-content-between" bg="dark" variant="dark">
           <Navbar.Brand>SEER Search</Navbar.Brand>
           <MessageForm
-          ref='messageFormRef'
-          searchMessage={this.searchMessage}
+            ref='messageFormRef'
+            searchMessage={this.searchMessage}
           />
         </Navbar>
-      <ErrorHandler
-      error={this.state.error}
-      />
-      <Container>
-      <Row>
-        <Col sm={10}>
-          <MessageList
-          messages={this.state.articles}
-          handleDelete={this.deleteMessage}
-          sendUpdate={this.sendUpdate}
-          clearSearch={this.getAllMessages}
-          />
-        </Col>
-        <Col sm={2}>
-        <DropdownButton style={divStyle} id="dropdown-basic-button" title="Sort">
-          <Dropdown.Item onClick={() =>{this.orderByYearDesc();}}>Sort by Newest</Dropdown.Item>
-          <Dropdown.Item onClick={() =>{this.orderByYearAsc();}}>Sort by Oldest</Dropdown.Item>
-          <Dropdown.Item onClick={() =>{this.orderByTitleAlphabetical();}}>Sort by title</Dropdown.Item>
-          <Dropdown.Item onClick={() =>{this.orderByAuthorAlphabetical();}}>Sort by author</Dropdown.Item>
-          <Dropdown.Item onClick={() =>{this.orderBySourceAlphabetical();}}>Sort by source</Dropdown.Item>
-        </DropdownButton>
-        </Col>
-      </Row>
-      </Container>
-      <SubmitArticleForm 
-        submitMessage={this.submitArticle}
-      />
+        <ErrorHandler
+          error={this.state.error}
+        />
+        <Container>
+          <Row>
+            <Col sm={10}>
+              <MessageList
+                messages={this.state.articles}
+                handleDelete={this.deleteMessage}
+                sendUpdate={this.sendUpdate}
+                clearSearch={this.getAllMessages}
+              />
+            </Col>
+            <Col sm={2}>
+              <DropdownButton style={divStyle} id="dropdown-basic-button" title="Sort">
+                <Dropdown.Item onClick={() => { this.orderByYearDesc(); }}>Sort by Newest</Dropdown.Item>
+                <Dropdown.Item onClick={() => { this.orderByYearAsc(); }}>Sort by Oldest</Dropdown.Item>
+                <Dropdown.Item onClick={() => { this.orderByTitleAlphabetical(); }}>Sort by title</Dropdown.Item>
+                <Dropdown.Item onClick={() => { this.orderByTitleAlphabeticalReverse(); }}>Sort by title reverse</Dropdown.Item>
+                <Dropdown.Item onClick={() => { this.orderByAuthorAlphabetical(); }}>Sort by author</Dropdown.Item>
+                <Dropdown.Item onClick={() => { this.orderByAuthorAlphabeticalReverse(); }}>Sort by author reverse</Dropdown.Item>
+                <Dropdown.Item onClick={() => { this.orderBySourceAlphabetical(); }}>Sort by source</Dropdown.Item>
+                <Dropdown.Item onClick={() => { this.orderBySourceAlphabeticalReverse(); }}>Sort by source reverse</Dropdown.Item>
+              </DropdownButton>
+            </Col>
+          </Row>
+        </Container>
+        <SubmitArticleForm
+          submitMessage={this.submitArticle}
+        />
       </React.Fragment>
     );
   }
