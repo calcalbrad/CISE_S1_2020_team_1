e70c9e9c16cd91f5676b8848dcfae4bcb2a7498d
Callum <callumbradding@gmail.com>
2020-06-03 16:01:51 +1200

    Updated messageList.test.js unit tests pass

--- frontend/src/tests/messageList.test.js
+++ frontend/src/tests/messageList.test.js
@@ -5,19 +5,22 @@ import Adapter from 'enzyme-adapter-react-16';
 import { mount, shallow } from 'enzyme'
 import mockMessages from '../__mocks__/messages.json'
 Enzyme.configure({ adapter: new Adapter()})
-describe('List', () => {
+describe('Article List', () => {
   it('renders without crashing', () => {
     const component = mount(<MessageList/>);
     expect(component).toMatchSnapshot();
   });
+
   it('takes messages as props and displays them', () => {
     const component = shallow(<MessageList messages={mockMessages}/>);
     expect(component.find('ul#message_list').length).toBe(1);
   });
+
   it('each message in list has delete button', () => {
     const component = mount(<MessageList messages={mockMessages}/>);
     expect(component.find('ul#message_list').childAt(0).exists('button#delete')).toBe(true);
   });
+
   it('each message has update button', () => {
     const component = shallow(<MessageList messages={mockMessages} loaded={true} />)
     expect(component.find('ul#message_list').childAt(0).find('#update').text()).toBe('update')
@@ -31,6 +34,5 @@ describe('List', () => {
       component.find('ul#message_list').childAt(0).find('#update').simulate('click')
       expect(component.find('ul#message_list').childAt(0).find('#updateBox').text()).toBe('Hello')
       expect(component.find('ul#message_list').childAt(0).find('#send').text()).toBe('Send Update')
-    });
-
   });
+});
