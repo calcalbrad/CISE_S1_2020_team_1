cbbeb618f5d402a21c71b13334e2069f728170b3
Callum <callumbradding@gmail.com>
2020-06-12 14:22:42 +1200

    Updated searcForm and removed MessageForm

--- frontend/src/tests/searchForm.test.js
+++ frontend/src/tests/searchForm.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MessageForm from '../components/MessageForm'
+import SearchForm from '../components/searchForm'
 import Enzyme, { render, ReactWrapper } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { mount, shallow } from 'enzyme'
