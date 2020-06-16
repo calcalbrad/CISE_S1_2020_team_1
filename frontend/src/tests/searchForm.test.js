7b31ad7f81d4f9ff05b48bccc7402a7b3e9df40c
Callum <callumbradding@gmail.com>
2020-06-16 15:41:29 +1200

    Updated tests in searchForm.test.js

--- frontend/src/tests/searchForm.test.js
+++ frontend/src/tests/searchForm.test.js
@@ -5,46 +5,67 @@ import Enzyme, { render, ReactWrapper } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { mount, shallow } from 'enzyme'
 import { getByTestId, fireEvent } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 Enzyme.configure({ adapter: new Adapter()})
+
+let container = null;
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  ReactDOM.unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+})
 
 describe('Search Form', () => {
 
   it('renders without crashing', () => {
-    const component = mount(<MessageForm/>);
+    const component = mount(<SearchForm/>);
     expect(component).toMatchSnapshot();
   });
 
   it('has textbox', () => {
-    const component = mount(<MessageForm/>);
+    const component = mount(<SearchForm/>);
     expect(component.exists('textarea#message_box'));
   });
 
   it('has submit button', () => {
-    const component = mount(<MessageForm/>);
+    const component = mount(<SearchForm/>);
     expect(component.exists('button#submit'));
   });
 
-  it('should update state message when text entered', () => {
-    //const component = shallow(<MessageForm/>);
-    //component.find('textarea#message_box').simulate('change', { target: { value: 'Hello' } });
-    //expect(component.state('currentMessage')).toEqual('Hello');
-
-    const container = mount(<MessageForm/>);
-    const text_box = container.find('message_box')
-    text_box.simulate('change', {target: { value: 'Hello' } });
-    expect(component.state('currentMessage').toEqual('Hello'));
-  });
-
-  it('clears message box on submit', () => {
-    const component = mount(<MessageForm
-      submitMessage={function(item){return true}}
+  it('should update title state', () => {
+    var component = mount(<SearchForm
+      topics={'testtopic1', 'testtopic2'}
     />);
-    component.find('textarea#message_box').simulate('change', { target: { value: 'Hello' } })
-    expect(component.state('currentMessage')).toEqual('Hello');
-    component.find('form').simulate('submit')
-    component.update()
-    expect(component.find('textarea#message_box').props().value).toEqual('');
-    expect(component.state('currentMessage')).toEqual('');
+    component.setState({ title: 'Hello' })
+    expect(component.state('title')).toEqual('Hello');
   });
 
+  it('should update author state', () => {
+    var component = mount(<SearchForm
+      topics={'testtopic1', 'testtopic2'}
+    />);
+    component.setState({ author: 'Hello' })
+    expect(component.state('author')).toEqual('Hello');
+  });
+
+  it('should update topics state', () => {
+    var component = mount(<SearchForm
+      topics={'testtopic1', 'testtopic2'}
+    />);
+    component.setState({ topics: 'testtopic'})
+    expect(component.state('topics')).toEqual('testtopic');
+  });
+
+  it('should update source state', () => {
+    var component = mount(<SearchForm
+      topics={'testtopic1', 'testtopic2'}
+    />);
+    component.setState({ source: 'Hello'})
+    expect(component.state('source')).toEqual('Hello');
+  });
 });
